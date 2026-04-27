dc4b5c738c4ed3a8ae779df5a02879e2a7d0113d
Joseph200371 <jm_espinola_a2003@live.com.ar>
2026-04-27 18:43:07 -0300

    feat: add exercise A1_02 sum input numbers

---
+++ src/fundamentals/A1_02_sum_input_numbers.py
@@ -0,0 +1,15 @@
+print("\nA1 - Ejercicio 2:\n")
+print("Sumar dos números cualquiera y mostrar el resultado\n")
+
+numero1 = int(input("Ingrese el primer número: "))      # pedimos el primer número y lo convertimos a entero
+
+numero2 = int(input("Ingrese el segundo número: "))     # pedimos el segundo número y lo convertimos a entero
+
+# realizamos la suma entre las variables de numero1 y numero2
+resultado = numero1 + numero2 
+
+print("\nSuma: ", numero1, "+", numero2)                # realizamos la suma en la pantalla
+
+input("\nPresione Enter para continuar...\n")           # pausamos la ejecución
+
+print("El resultado es: ", resultado, "\n")             # mostramos el resultado 
