d50aae6738f55b003d044f6816ca4a0e94547c73
Joseph200371 <jm_espinola_a2003@live.com.ar>
2026-04-27 18:39:59 -0300

    feat: add exercise A1_01 sum fixed numbers

---
+++ src/fundamentals/A1_01_sum_fixed_numbers.py
@@ -0,0 +1,23 @@
+print("\nA1 - Ejercicio 1: \n")
+print("Sumar dos números 2 y 5. Mostrar el resultado en pantalla.\n")
+print("El resultado de la suma es:")
+print(2+5)
+
+print("\nA1 - Ejercicio 1.1: Usando variables \n")
+
+numero1 = 2                     # guardamos el número 2 en una variable
+numero2 = 5                     # guardamos el número 5 en otra variable
+
+resultado = numero1 + numero2   # sumamos las variables y guardamos el resultado en una nueva variable
+
+print("resultado")              # muestra la palabra resultado
+print(resultado)                # muestra el valor de la variable resultado
+print("\n")                     # Salto de línea para mejorar la legibilidad
+
+print("El resultado de la suma es:")
+print(resultado)                # mostramos el resultado
+print("\n")
+
+print("Otra forma de mostrar el resultado es:")
+print(numero1, "+", numero2, "=", resultado)  # mostramos el resultado de una forma más completa
+print("\n")
