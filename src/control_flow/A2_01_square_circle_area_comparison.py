55ca50447349063661c7f4a2a48addfa81b3a73b
Joseph200371 <jm_espinola_a2003@live.com.ar>
2026-04-30 15:49:30 -0300

    feat: add exercise A2_01 square and circle area comparison

---
+++ src/control_flow/A2_01_square_circle_area_comparison.py
@@ -0,0 +1,38 @@
+print("\nA2 - Ejercicio 1:\n")
+print ("""- Se conocen dos números distintos:
+Calcular la superficie de un cuadrado, suponiendo como lado del mismo al mayor de los números dados 
+    y la superficie de un círculo suponiendo como radio del mismo al menor de los números dados.\n""")
+
+# --- ENTRADA DE DATOS ---
+# Valor fijo de pi
+pi = 3.14
+
+numero_1 = float(input("- Ingrese el primer número: "))
+
+numero_2 = float(input("- Ingrese el segundo número: "))
+
+# --- VALIDACIÓN Y PROCESO ---
+if numero_1 > numero_2:            # Se evalua si el numero_1 es mayor al numero_2
+    num_mayor = numero_1
+    num_menor = numero_2
+else:
+    # Determinar mayor y menor
+    if  numero_2 > numero_1:       # Se evalua si el numero_2 es mayor al numero_1
+        num_mayor = numero_2
+        num_menor = numero_1
+
+# Cálculos
+# Se cálcula las superficie para Cuadrado y circulo
+superficie_circulo = pi * (num_menor ** 2)
+superficie_cuadrado = num_mayor ** 2
+
+# --- SALIDA DE DATOS ---
+print("\n==================================================")
+print("RESULTADOS")
+print("==================================================")
+print(f"Lado del cuadrado (mayor): {num_mayor}")
+print(f"Radio del círculo (menor): {num_menor}")
+print("--------------------------------------------------")
+print(f"Superficie del cuadrado : {superficie_cuadrado:.2f}")
+print(f"Superficie del círculo  : {superficie_circulo:.2f}")
+print("==================================================\n")
